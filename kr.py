2e9bf07e591c08facd83ac8c92cc4f1d4fba71ea
Rihler <ermakoffsergey@gmail.com>
2023-10-05 23:57:38 +0300

    win

--- kr.py
+++ kr.py
@@ -12,6 +12,17 @@ class Field(object):
             "C2": [2, 1],
             "C3": [2, 2],
         }
+        self.sym ={
+            "A1": 0,
+            "A2": 0,
+            "A3": 0,
+            "B1": 0,
+            "B2": 0,
+            "B3": 0,
+            "C1": 0,
+            "C2": 0,
+            "C3": 0,
+        }
 
     def print_field(self):
         print('-' * 14)
@@ -23,14 +34,14 @@ class Field(object):
     def change(self, sym, x):
         while x not in self.cords.keys():
             x = input("Такой координаты нет, введите те, которые доступны на поле: ")
-        if self.cords[x][-1] != "+":
+        if self.sym[x] != 'O' or self.sym[x] !="X":
             pass
         else:
-            while self.cords[x][-1] == "+":
+            while self.sym[x][-1] == 'O' or self.sym[x][-1] == "X":
                 x = input("Данная клетка занята, выберите другую: ")
-                while x not in self.cords.keys():
+                while x not in self.sym.keys():
                     x = input("Такой координаты нет, введите те, которые доступны на поле: ")
-        self.cords[x].append("+")
+        self.sym[x] = sym
         x1 = self.cords[x][0]
         y1 = self.cords[x][1]
         self.field[x1][y1] = f"|{sym}|"
@@ -44,17 +55,39 @@ def change_sym(name):
         syms = input("1. X\n2. O\n")
     return "X" if syms == "1" else "O"
 
+def checkWinner(lst, sym):
+    winner = 0
+    win_lst = [["A1", "A2", "A3"],
+               ["B1", "B2", "B3"],
+               ["C1", "C2", "C3"]
+               ["A1", "B2", "C3"],
+               ["A3", "B2", "C1"],
+               ["A1","B1", "C1"],
+               ["A2","B2", "C2"],
+               ["A3", "B3", "C3"]]
+    
+    for i in range(8):
+        if lst[win_lst[i][0]] == lst[win_lst[i][1]] == lst[win_lst[i][2]]\
+              and lst[win_lst[i][0]] != 0:
+            winner = lst[win_lst[i][0]]
+            break
+    return winner
+
 def game(name1, name2):
     ex = True
     kr = Field()
     kr.print_field()
-    nsym1 = change_sym(name1)
-    nsym2 = "O" if nsym1 == "X" else "X"
+    players ={}
+    sym1 = change_sym(name1)
+    sym2 = "O" if sym1 == "X" else "X"
+    players[sym1] = name1
+    players[sym2] = name2
     while ex:
-        kr.change(nsym1, input(f"Введите координату, {name1} : "))
+        kr.change(players[name1], input(f"Введите координату, {name1} : "))
         kr.print_field()
-        kr.change(nsym2, input(f"Введите координату, {name2}: "))
+        kr.change(players[name2], input(f"Введите координату, {name2}: "))
         kr.print_field()
+        checkWinner(kr.sym, players[name2])
 
 game(input("Имя 1-го игрока: "), input("Имя 2-го игрока: "))
 
